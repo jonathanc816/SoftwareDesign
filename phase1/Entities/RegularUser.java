583c93dc0d170b4aae48d22d8ea5bc7f1a81eaae
jonathanc816 <55709123+jonathanc816@users.noreply.github.com>
2021-07-14 03:43:59 +0800

    "Implemented some methods in the Use Case classes(usedUsername, createUser) and initiate an ArrayList to store users"

--- phase1/Entities/RegularUser.java
+++ phase1/Entities/RegularUser.java
@@ -1,5 +1,3 @@
-import java.util.Hashtable;
-
 public class RegularUser extends User {
     public RegularUser(String username, String password){
         super(username, password);
--- phase1/UseClass/UserManager.java
+++ phase1/UseClass/UserManager.java
@@ -1,2 +1,31 @@
-public class UserManager {
+import java.util.ArrayList;
+
+public class UserManager{
+    /**
+     * Stores users (ArrayList)
+     * Create an User object
+     * Duplicate (Registered) User checker
+     */
+
+    private ArrayList<User> users;
+
+    public boolean usedUsername(User username){
+        return this.users.contains(username);
+    }
+
+    // I decided to exclude guest since the data of a guest user is temporary and therefore should not be added
+    //to the list
+    public User createUser(String username, String password, String type){
+        if (type.equals("Regular")){
+            User rUser = new RegularUser(username,password);
+            this.users.add(rUser);
+            return rUser;
+        }
+        else if (type.equals("Admin")){
+            User aUser = new AdminUser(username,password);
+            this.users.add(aUser);
+            return aUser;
+        }
+        return null;
+    }
 }
