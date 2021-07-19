cad03e600ee6861750df4c16a1a4e7fb692af7aa
fmt47 <changyp01@gmail.com>
2021-07-19 19:27:36 -0400

    "implement TemplateManager.java with methods"

--- phase1/UseClass/TemplateManager.java
+++ phase1/UseClass/TemplateManager.java
@@ -1,2 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This the class stores and modifies the template abjects
+ *
+ * @author  Yupeng Chang
+ * @version 1.0
+ * @since   2020-07-19
+ */
 public class TemplateManager {
+    private List<Template> templates = new ArrayList<>();
+
+    /**
+     * The method return names of all templates as a list
+     */
+    public ArrayList<String> getAllNames() {
+        ArrayList<String> nameList = new ArrayList<>();
+        for (Template template : templates) {
+            nameList.add(template.getName());
+        }
+        return nameList;
+    }
+
+    /**
+     * The method add one new template to the template list
+     * @param template This is the new Template object
+     */
+    public void addTemplate(Template template) {
+        templates.add(template);
+    }
+
+    /**
+     * The method add several new templates to the template list
+     * @param newTemplates This is list of new Template objects
+     */
+    public void addTemplate(List<Template> newTemplates) {
+        templates.addAll(newTemplates);
+    }
+
+    /**
+     * The getter method of a certain template
+     * @param name This is the name of gotten object
+     */
+    public  Template getTemplate(String name) {
+        for(Template template: templates) {
+            if (template.getName().equals(name)) {
+                return template;
+            }
+        }
+        return new Template("", "", 0, "", new String[0]);
+    }
+
+    /**
+     * The method modify the information of selected template
+     * @param name This is the name of selected object
+     * @param newInfo This is the new information
+     */
+    public void setTemplatesInfo(String name, String newInfo) {
+        for (Template template : templates) {
+            if (template.getName().equals(name)) {
+                template.setInfo(newInfo);
+            }
+        }
+    }
 }
