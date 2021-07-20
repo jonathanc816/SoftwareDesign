ce575ca9180ee653df3fbbccdfbbdd3bae95b2f4
JustinZ17 <jz17392@gmail.com>
2021-07-20 01:17:46 -0700

    Added contact info to contacts.txt

--- phase1/Entities/Pet.java
+++ phase1/Entities/Pet.java
@@ -1,79 +1,78 @@
 public class Pet {
-    private String petName;
-    private final int id;
-    private final String petColour;
-    private final String petSex;
-    private boolean publicity;
-    private int hungerLevel;
-    private int growthLevel;
-    private String status;
+  private String petName;
+  private final int id;
+  private final String petColour;
+  private final String petSex;
+  private boolean publicity;
+  private int hungerLevel;
+  private int growthLevel;
+  private String status;
 
+  public Pet(
+      String petName, int id, String petColour, String petSex, boolean publicity, String status) {
+    this.petName = petName;
+    this.petColour = petColour;
+    this.petSex = petSex;
+    this.publicity = publicity;
+    this.hungerLevel = 10;
+    this.growthLevel = 0;
+    this.id = id;
+    this.status = status;
+  }
 
-    public Pet(String petName, int id, String petColour, String petSex, boolean publicity, String status) {
-        this.petName = petName;
-        this.petColour = petColour;
-        this.petSex = petSex;
-        this.publicity = publicity;
-        this.hungerLevel = 10;
-        this.growthLevel = 0;
-        this.id = id;
-        this.status = status;
-    }
+  public String getPetName() {
+    return this.petName;
+  }
 
-    public String getPetName(){
-        return this.petName;
-    }
+  public void setPetName(String newName) {
+    this.petName = newName;
+  }
 
-    public void setPetName(String newName){
-        this.petName = newName;
-    }
+  public int getId() {
+    return this.id;
+  }
 
-    public int getId(){
-        return this.id;
-    }
+  public String getPetColour() {
+    return this.petColour;
+  }
 
-    public String getPetColour(){
-        return this.petColour;
-    }
+  public String getPetSex() {
+    return this.petSex;
+  }
 
-    public String getPetSex(){
-        return this.petSex;
-    }
+  public boolean getPublicity() {
+    return this.publicity;
+  }
 
-    public boolean getPublicity(){
-        return this.publicity;
-    }
+  public void setPublicity(boolean newPublicity) {
+    this.publicity = newPublicity;
+  }
 
-    public void setPublicity(boolean newPublicity){
-        this.publicity = newPublicity;
-    }
+  public int getHungerLevel() {
+    return this.hungerLevel;
+  }
 
-    public int getHungerLevel(){
-        return this.hungerLevel;
-    }
+  public void increaseHungerLevel(int level) {
+    this.hungerLevel = Math.min(10, this.hungerLevel + level);
+  }
 
-    public void increaseHungerLevel(int level){
-        this.hungerLevel = Math.min(10, this.hungerLevel + level);
-    }
+  public void decreaseHungerLevel(int level) {
+    this.hungerLevel = Math.max(0, this.hungerLevel - level);
+  }
 
-    public void decreaseHungerLevel(int level){
-        this.hungerLevel = Math.max(0, this.hungerLevel - level);
-    }
+  public int getGrowthLevel() {
+    return this.growthLevel;
+  }
 
-    public int getGrowthLevel(){
-        return this.growthLevel;
-    }
+  public void increaseGrowthLevel() {
+    this.growthLevel += 1;
+  }
 
-    public void increaseGrowthLevel(){
-        this.growthLevel += 1;
-    }
+  public String getStatus() {
+    return this.status;
+  }
 
-    public String getStatus(){
-        return this.status;
-    }
-
-    public void setStatus(String newStatus){
-        this.status = newStatus;
-    }
-
+  public void setStatus(String newStatus) {
+    this.status = newStatus;
+  }
 }
--- phase1/UseClass/PetManager.java
+++ phase1/UseClass/PetManager.java
@@ -2,86 +2,82 @@ import java.util.List;
 
 /**
  * This is the class that stores and manages Pet objects
+ *
  * @author Zhi Heng(Justin) Zheng
  */
-
 public class PetManager {
 
-    private List<Pet> petList;
+  private List<Pet> petList;
 
-    /**
-     * This method returns the list of Pet objects stored
-     */
-    public List<Pet> getPetList() {
-        return petList;
+  /** This method returns the list of Pet objects stored */
+  public List<Pet> getPetList() {
+    return petList;
+  }
+
+  /**
+   * This method changes petList based on the list given
+   *
+   * @param newPetList this is the new list that will replace the existing petList
+   */
+  public void setPetList(List<Pet> newPetList) {
+    this.petList = newPetList;
+  }
+
+  /**
+   * This method adds one Pet object to petList
+   *
+   * @param newPet this is the new Pet object
+   */
+  public void addPet(Pet newPet) {
+    this.petList.add(newPet);
+  }
+
+  /**
+   * This method searches petList for a Pet object with petID and returns the object If it fails to
+   * find such Pet object, it will return null
+   *
+   * @param petID this is the ID of the pet we are looking for
+   */
+  public Pet findPet(int petID) {
+    for (Pet pet : petList) {
+      if (pet.getId() == petID) {
+        return pet;
+      }
     }
+    return null;
+  }
 
-    /**
-     * This method changes petList based on the list given
-     *
-     * @param newPetList this is the new list that will replace the existing petList
-     */
-    public void setPetList(List<Pet> newPetList) {
-        this.petList = newPetList;
+  /** This method creates a new Pet object, adds it to petList, and returns it */
+  public Pet createPet(
+      String petName, int id, String petColour, String petSex, boolean publicity, String status) {
+    Pet newPet = new Pet(petName, id, petColour, petSex, publicity, status);
+    addPet(newPet);
+    return newPet;
+  }
+
+  /**
+   * This method checks if the Pet object is public or private
+   *
+   * @param pet This is the Pet object to check
+   */
+  public String checkPublicity(Pet pet) {
+    if (pet.getPublicity()) {
+      return "This pet is public.";
+    } else {
+      return "This pet is private.";
     }
+  }
 
-    /**
-     * This method adds one Pet object to petList
-     *
-     * @param newPet this is the new Pet object
-     */
-    public void addPet(Pet newPet) {
-        this.petList.add(newPet);
+  /**
+   * This method feeds the Pet object by the amount provided
+   *
+   * @param petID This is the Pet object to feed
+   * @param amount This is the amount we want to feed
+   */
+  public void feedPet(int petID, int amount) {
+    var petToFeed = findPet(petID);
+    if (petToFeed != null) {
+      petToFeed.increaseHungerLevel(amount);
     }
-
-    /**
-     * This method searches petList for a Pet object with petID and returns the object
-     * If it fails to find such Pet object, it will return null
-     *
-     * @param petID this is the ID of the pet we are looking for
-     */
-    public Pet findPet(int petID) {
-        for (Pet pet : petList) {
-            if (pet.getId() == petID) {
-                return pet;
-            }
-        }
-        return null;
-    }
-
-    /**
-     * This method creates a new Pet object, adds it to petList, and returns it
-     */
-    public Pet createPet(String petName, int id, String petColour, String petSex, boolean publicity, String status){
-        Pet newPet = new Pet(petName, id, petColour, petSex, publicity, status);
-        addPet(newPet);
-        return newPet;
-    }
-
-    /**
-     * This method checks if the Pet object is public or private
-     *
-     * @param pet This is the Pet object to check
-     */
-    public String checkPublicity(Pet pet){
-        if (pet.getPublicity()){
-            return "This pet is public.";
-        } else{
-            return "This pet is private.";
-        }
-    }
-
-    /**
-     * This method feeds the Pet object by the amount provided
-     *
-     * @param petID This is the Pet object to feed
-     * @param amount This is the amount we want to feed
-     */
-    public void feedPet(int petID, int amount){
-        var petToFeed = findPet(petID);
-        if (petToFeed != null) {
-            petToFeed.increaseHungerLevel(amount);
-        }
-    }
-
+  }
 }
