2e7393ed7998ee6f59e92fa795f6784f7b386ae2
Kelvin <k.jiang@mail.utoronto.ca>
2021-07-23 00:33:29 -0700

    changed structure of messagemanager

--- phase1/UseClass/MessageManager.java
+++ phase1/UseClass/MessageManager.java
@@ -9,31 +9,20 @@ import java.util.HashMap;
  */
 public class MessageManager{
 
-    private HashMap<String, ArrayList<String>> messageDict;
-    private static final HashMap<String, HashMap<String, ArrayList<String>>> allMessageDicts = new HashMap<>();
+    private static Integer messageID;
+    public HashMap<Integer, String> messageDict;
 
-    public MessageManager(User userID){
+    public MessageManager(){
         this.messageDict = new HashMap<>();
-        allMessageDicts.put(userID.getUsername(), this.messageDict);
-    }
-
-    public void setMessageDict(User userID){
-        for (int i=0; i<= userID.getFriendList().size(); i++) {
-            this.messageDict.put(userID.getFriendList().get(i), new ArrayList<>());
-        }
+        messageID = 0;
     }
 
     public void addMessage(Message newMessage){
-        allMessageDicts.get(newMessage.getFromID()).get(newMessage.getToID()).add(newMessage.getContent());
-        allMessageDicts.get(newMessage.getToID()).get(newMessage.getFromID()).add(newMessage.getContent());
+        this.messageDict.put(messageID, newMessage.getContent());
     }
 
-    public HashMap<String, ArrayList<String>> getMessagesFor(String id){
-        return allMessageDicts.get(id);
-    }
-
-    public ArrayList<String> getMessageFrom(String userIDMain, String otherUserID){
-        return allMessageDicts.get(userIDMain).get(otherUserID);
+    public String getMessage(Integer messageID){
+        return messageDict.get(messageID);
     }
 
 
