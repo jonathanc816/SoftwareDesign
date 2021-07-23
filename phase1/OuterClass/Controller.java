edc36075263bdc14a77dd13ed607b7fb652057c0
fmt47 <changyp01@gmail.com>
2021-07-23 17:09:16 -0400

    "Implement Presenter and Controller"

--- phase1/OuterClass/Controller.java
+++ phase1/OuterClass/Controller.java
@@ -1,2 +1,47 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Controller {
-}
+    static public String getUserString() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+        }
+
+    static public int getUserNum(int maxNum) {
+        ArrayList<String> choices = new ArrayList<>();
+        for (int i=1; i<maxNum+1; i++) {
+            choices.add(Integer.toString(i));
+        }
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String userChoice = sc.nextLine();
+            if (choices.contains(userChoice)) {
+                return Integer.parseInt(userChoice);
+                }
+            System.out.println("Invalid number, please enter again");
+        }
+
+    }
+
+    static public int getUserNum() {
+        return getUserNum(999);
+    }
+
+    static public String getUserString(String instruction) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(instruction);
+        return sc.nextLine();
+    }
+
+    static public void starterMenu() {
+        Presenter.showMenu(new String[]{"Login", "Create a new user"}, "Welcome to Tamagochi, enter number to...");
+        int input = getUserNum(2);
+        if (input == 1) {
+            System.out.println("Login");
+            }
+        if (input == 2) {
+            System.out.println("Create");
+            }
+        }
+    }
--- phase1/OuterClass/Presenter.java
+++ phase1/OuterClass/Presenter.java
@@ -1,2 +1,18 @@
 public class Presenter {
+    static public void showMenu(String[] menu) {
+        int i = 1;
+        for (String line : menu){
+            System.out.println(i+". "+line);
+            i ++;
+        }
+    }
+
+    static public void showMenu(String[] menu, String title) {
+        System.out.println(title);
+        int i = 1;
+        for (String line : menu){
+            System.out.println(i+". "+line);
+            i ++;
+        }
+    }
 }
